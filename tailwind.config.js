a5f75d4773e2fa0186406c91c5d6ab3bf4251f5f
Azamjon <azamjonkobulov1998@gmail.com>
2025-01-15 23:32:32 +0500

    add/second page

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,11 +34,16 @@ module.exports = {
             300: "#908EB5",
             400: "#303070",
             500: "#3C3B75",
+            600: "#7C799D",
           },
           green: "#25BB7C",
           gray: {
             DEFAULT: "#A29C9A",
             100: "#9D9DA6",
+            200: "#CAC7DD",
+            300: "#F2F2F2",
+            400: "#737097",
+            500: "#5C5A78",
           },
           brown: {
             DEFAULT: "#978686",
@@ -51,6 +56,11 @@ module.exports = {
       },
       backgroundImage: {
         base: "linear-gradient(86.41deg, #191D42 0%, #192D51 6.5%, #321E59 19%, #321E59 33%, #363583 60%, #321E59 87%, #1C2B52 100%);",
+        ssl: "linear-gradient(180deg, #18183E 0%, #2B295A 100%);",
+        "comparison-2": "linear-gradient(90deg, #C638FF 0%, #5D92E2 100%);",
+        "comparison-3": "linear-gradient(90deg, #BD1F76 0%, #6C53D4 100%);",
+        "comparison-4": "linear-gradient(90deg, #382A87 0%, #7A6EDA 100%);",
+        "comparison-5": "linear-gradient(90deg, #1A90CD 0%, #3BDDD0 100%);",
       },
     },
   },
